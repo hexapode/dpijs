1c2e9becd9312a682031dfc89ccb17afeafe129c
Pierre <doulcet@gmail.com>
2012-05-13 09:54:05 +0200

    Remove unusefull function, just keep getDPI()

--- dpi.js
+++ dpi.js
@@ -28,16 +28,16 @@
 			 	[960, 640, 326]
 			 ],
 			 // HTC
-			 ['HTC Sensation Build', [960, 540, 256]],
-			 ['HTC_IncredibleS', [800,480, 252]],
-			 ['HTC Vision Build',[800, 480, 252]],
-			 ['HTC Desire Build', [800, 480, 252]],
-			 ['HTC_Pyramid', [960, 540, 256]],
-			 ['HTC_DesireS', [800, 480, 252]],
-	 		 ['HTC Legend Build', [480, 320, 180]],
-	 		 ['HTC_DesireHD', [800, 480, 217]],
-	 		 ['HTC_DesireZ', [800, 480, 252]],
-	 		 ['HTC_Wildfire', [320, 240, 125]]
+			 ['HTC Sensation Build', [256]],
+			 ['HTC_IncredibleS', [252]],
+			 ['HTC Vision Build',[252]],
+			 ['HTC Desire Build', [252]],
+			 ['HTC_Pyramid', [256]],
+			 ['HTC_DesireS', [252]],
+	 		 ['HTC Legend Build', [180]],
+	 		 ['HTC_DesireHD', [217]],
+	 		 ['HTC_DesireZ', [252]],
+	 		 ['HTC_Wildfire', [125]]
 		]
 
 		var phoneDPI = null;
@@ -47,6 +47,7 @@
 			var i = 0;
 			var x = screen.width;
 			var y = screen.height;
+			var d = data;
 
 			if (x < y)
 			{
@@ -56,15 +57,20 @@
 			}
 			while (i < data.length)
 			{
-				var patt=new RegExp(data[i][0],'i');
+				var patt=new RegExp(d[i][0],'i');
 				if (navigator.userAgent.match(patt))
 				{
-					var j = 0;
-					while (j < data[i][1].length)
+					var j = 1;
+					while (j < d[i].length)
 					{
-						if (data[i][1][j][0] == x && data[i][1][j][1] == y)
+						if (d[i][j].length > 2 && d[i][j][0] == x && data[i][j][1] == y)
 						{
-							phoneDPI = data[i][1][j][2]
+							phoneDPI = d[i][j][2]
+							return;
+						}
+						else if (data[i][j].length < 2)
+						{
+							phoneDPI = d[i][j][0]
 							return;
 						}
 						++j;
@@ -74,38 +80,6 @@
 			}
 			phoneDPI = 96;
 		}
-
-		function getMmFromPixel(pixels)
-		{
-			if (!phoneDPI)
-				detectPhone();
-			return (pixels / phoneDPI) / 0.393700787 | 0;
-		}
-		window.getMmFromPixel = getMmFromPixel;
-
-		function getPixelFromMm(mm)
-		{
-			if (!phoneDPI)
-				detectPhone();
-			return mm * 0.393700787 * phoneDPI | 0;
-		}
-		window.getPixelFromMm = getPixelFromMm;
-
-		function getInchFromPixel(pixels)
-		{
-			if (!phoneDPI)
-				detectPhone();
-			return (pixels / phoneDPI) | 0;
-		}
-		window.getInchFromPixel = getInchFromPixel;
-
-		function getPixelFromInch(inch)
-		{
-			if (!phoneDPI)
-				detectPhone();
-			return inch  * phoneDPI | 0;
-		}
-		window.getPixelFromInch = getPixelFromInch;
 
 		function getDPI()
 		{
